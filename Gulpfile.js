f83c31e5e8899f583507a4788036e6e6adf63efd
Troy Nichols <troygnichols@gmail.com>
2016-04-21 18:07:56 -0500

    Set up browserify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,19 @@
 "use strict";
 
-var gulp    = require('gulp');
-var connect = require('gulp-connect');
-var open    = require('gulp-open');
+var gulp       = require('gulp');
+var connect    = require('gulp-connect');
+var open       = require('gulp-open');
+var browserify = require('browserify');
+var reactify   = require('reactify');
+var source     = require('vinyl-source-stream');
 
 var config = {
   port: 9005,
   devBaseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
+    js:   './src/**/.js',
+    mainJs: './src/main.js'
     dist: './dist'
   }
 };
@@ -37,8 +42,19 @@ gulp.task('html', function() {
     .pipe(connect.reload());
 });
 
+gulp.task('js', function() {
+  browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle() // put it all into one file
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
+})
+
 gulp.task('watch', function() {
   gulp.watch(config.paths.html, ['html']);
+  gulp.watch(config.paths.mainJs, ['js']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
